00dc0fbb8013503e769c9a898d53bf6151d8aee7
kenniajahani <garciakennia84@gmail.com>
2025-02-27 10:21:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -45,20 +45,57 @@ def app2():
 
     return "<h5>Hola, soy la view app</h5>"
 
-@app.route("/productos")
-def productos():
+@app.route("/libros")
+def libros():
     if not con.is_connected():
         con.reconnect()
 
     cursor = con.cursor(dictionary=True)
     sql    = """
-    SELECT Id_Producto,
-           Nombre_Producto,
-           Precio,
-           Existencias
+    SELECT idLibro,
+           titulo,
+           autor,
+           fechaPublicacion,
+           resena
 
-    FROM productos
+    FROM libros
 
+    LIMIT 10 OFFSET 0
+    """
+
+    cursor.execute(sql)
+    registros = cursor.fetchall()
+
+    # Si manejas fechas y horas
+    for registro in registros:
+        fechaPublicacion = registro["fechaPublicacion"]
+
+        registro["fechaPublicacion"] = fecha_publicacion.strftime("%Y-%m-%d %H:%M:%S")
+        registro["Fecha"]      = fecha_publicacion.strftime("%d/%m/%Y")
+        registro["Hora"]       = fecha_publicacion.strftime("%H:%M:%S")
+
+    return render_template("libros.html", libros=registros)
+
+@app.route("/calificaciones")
+def calificaciones():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT p4_calificaciones.idCalificacion,
+           p4_calificaciones.idLibro,
+           p4_calificaciones.ip,
+           p4_calificaciones.calificacion,
+           libros.id_libro,
+           libros.titulo,
+           libros.autor,
+           libros.fecha_publicacion,
+           libros.resena
+
+    FROM p4_calificaciones
+    JOIN libros
+    ON p4_calificaciones.idLibro = libros.idLibro
     LIMIT 10 OFFSET 0
     """
 
@@ -75,7 +112,9 @@ def productos():
         registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
     """
 
-    return render_template("productos.html", productos=registros)
+    return render_template("calificaciones.html", calificaciones=registros)
+
+
 
 @app.route("/productos/buscar", methods=["GET"])
 def buscarProductos():
@@ -207,3 +246,7 @@ def eliminarProducto():
     con.close()
 
     return make_response(jsonify({}))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
